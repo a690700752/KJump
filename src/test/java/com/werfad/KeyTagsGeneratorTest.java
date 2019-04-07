cd4fb4a5fc7f7e39df9e8f7f750162cb8fd2f5ee
a690700752 <tz59pk@gmail.com>
2019-04-07 11:58:02 +0800

    Convert kotlin tests to java tests.

---
+++ src/test/java/com/werfad/KeyTagsGeneratorTest.java
@@ -0,0 +1,24 @@
+package com.werfad;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class KeyTagsGeneratorTest {
+
+    @Test
+    public void createTagsTree() {
+        List<String> res = KeyTagsGenerator.createTagsTree(756,
+                "abcdefghijklmnopqrstuvwxyz");
+
+        assertEquals(res.size(), 756);
+        System.out.println(res.size());
+        System.out.println(res);
+
+        List<String> res1 = KeyTagsGenerator.createTagsTree(0,
+                "abcdefghijklmnopqrstuvwxyz");
+        assertEquals(res1.size(), 0);
+    }
+}
